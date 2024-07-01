454650ca08616abf96e866bdd1839dd1ff91b7f8
codeabx8 <bhotiaarun8@gmail.com>
2024-07-01 15:19:19 +0545

    template and static

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("I am revision the django project again.")
+    query_string=request.GET
+    name=query_string.get("name")
+    print(name)
+    return HttpResponse(f"<h1>I am revision the django project again. I am also learning {name} </h1>")
+
+def  portfolio(request):
+    return render(request, template_name="myapp/portfolio.html")
